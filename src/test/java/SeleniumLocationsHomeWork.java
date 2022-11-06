adcda24f866a20ddcdb89dfcefa717a0b06ed021
Dana Shmite <dana.shmite@gmail.com>
2022-11-06 20:25:35 +0200

    Selenium Locations Home Work

---
+++ src/test/java/SeleniumLocationsHomeWork.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+
+public class SeleniumLocationsHomeWork {
+    private final By CURRENCY_BTN = By.id("currentCurrency");
+    private By SEARCH_NOW_BTN = By.xpath(//*[contains(@class, 'lg submit prevent')])
+
+    By.xpath(.//img[@src = 'https://www.discovercars.com/assets/desktop/img/svg/discovercars-blue.svg']);
+    browser.findElement(CURRENCY_BTN);
+    By.xpath(//*[contains(@class, 'link action my')]);
+    By.xpath(.//div[@class = 'home-caption']);
+    By.xpath(//*[contains(@class, 'Datepicker active')]);
+    By.xpath(.//span[@class='sb-residence-text']);
+    browser.findElement(SEARCH_NOW_BTN);
+    By.xpath(.//div[@class = 'tp-widget-review']);
+    By.xpath(//div[@class = 'row is-table-row']);
+
+
+    .//div[@class = 'car-top-filter-item car-groups-item filtering-event init']
+    .//div[@class = 'car-top-filter-control next']
+    .//span[@class='showing-cars']
+    .//div[@class = 'flt-type']
+    .//span[@class = 'min-price']
+    .//label[@for = 'fuel-type-ft-V']
+    .//label[@for = 'number-of-seats-seats-4']
+
+    By.linkText("http://www.discovercars.com/en/car-offer/rental-conditions/e6427d0d-77c7-42b2-946f-eb8af149a82f/6VNP/");
+    //*[contains(@class, 'btn btn-book submit click-on-car selected-car prevent-loading instant')]
+    .//span[@class = 'dc-ui badge badge-medium badge-blue']
+    .//div[@class = 'supplier-rating']
+    .//div[@class = 'price-item-price-main']
+}
